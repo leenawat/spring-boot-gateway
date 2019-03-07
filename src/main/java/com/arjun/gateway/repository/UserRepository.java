cc9a86a8ab2b5237070bc38800a9e44b2739cac3
Leenawat <leenawat.pa@gmail.com>
2019-03-07 14:25:13 +0700

    change to mysql

--- src/main/java/com/arjun/gateway/repository/UserRepository.java
+++ src/main/java/com/arjun/gateway/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.arjun.gateway.repository;
 
 import com.arjun.gateway.bean.auth.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User,String> {
-    @Query(value="{'email' : ?0}")
+public interface UserRepository extends JpaRepository<User, String> {
     User findByEmail(String email);
 }
